64647df27beddf95d9203a75130704610f5c844a
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-09-16 01:16:40 +0900

    REFACTOR : GenerateNumberTest setUp() 메서드 사용해 중복 로직 분리

--- src/test/java/baseball/number/GenerateNumberTest.java
+++ src/test/java/baseball/number/GenerateNumberTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
@@ -12,20 +13,23 @@ import org.junit.jupiter.api.Test;
 public class GenerateNumberTest {
 
     private final int GENERATE_NUMBER_COUNT = 3;
+    private Number number;
+
+    @BeforeEach
+    void setUp() {
+        number = new Number();
+        number.generateNumbers(GENERATE_NUMBER_COUNT);
+    }
 
     @DisplayName("숫자 3개 생성하기")
     @Test
     void generate_3_number() {
-        Number number = new Number();
-        number.generateNumbers(GENERATE_NUMBER_COUNT);
         assertThat(number.getSize()).isEqualTo(GENERATE_NUMBER_COUNT);
     }
 
     @DisplayName("중복된 숫자가 있는지 검증")
     @RepeatedTest(100)
     void duplicate_number_exist(){
-        Number number = new Number();
-        number.generateNumbers(GENERATE_NUMBER_COUNT);
         List<Integer> numbers = number.getNumbers();
         Set<Integer> tmpNumbers = new HashSet<>(numbers);
         assertThat(tmpNumbers.size()).isEqualTo(GENERATE_NUMBER_COUNT);
